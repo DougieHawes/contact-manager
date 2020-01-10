a5d69adeda34f15aa80ee675d6b029b89f20ff62
Dougie Hawes <dougiehawes@hotmail.com>
2020-01-10 21:11:09 +0000

    Updates

---
+++ precache-manifest.913abb1b2bf608eac9c0b744c9a2201d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17de1c5e245b6d41a20b87ea526612a8",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "c80ed6daed53fcff3cb5",
+    "url": "/contact-manager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ee64546aa0f4c08ea2b2",
+    "url": "/contact-manager/static/css/main.4d72dc57.chunk.css"
+  },
+  {
+    "revision": "c80ed6daed53fcff3cb5",
+    "url": "/contact-manager/static/js/2.a5520793.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contact-manager/static/js/2.a5520793.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee64546aa0f4c08ea2b2",
+    "url": "/contact-manager/static/js/main.519ef2f6.chunk.js"
+  },
+  {
+    "revision": "28a2d2006ff3061e3a06",
+    "url": "/contact-manager/static/js/runtime-main.e6b3bc96.js"
+  }
+]);
