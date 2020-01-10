d636c36f7d621d343186055df79898fd554e133f
Dougie Hawes <dougiehawes@hotmail.com>
2020-01-10 21:00:08 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Header from './components/layout/Header';
+import Contacts from './components/contacts/Contacts';
+import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
+import About from './components/Pages/About';
+import NotFound from './components/Pages/NotFound';
+
+import { Provider } from './context';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <Router>
+        <div className='App'>
+          <Header branding='Contact Manager' />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Contacts} />
+              <Route path='/contact/add' component={AddContact} />
+              <Route path='/contact/edit/:id' component={EditContact} />
+              <Route path='/about' component={About} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/components/contacts/Contact.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Consumer } from '../../context.js';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  onDeleteClick = async (id, dispatch) => {
+    try {
+      await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+      dispatch({ type: 'DELETE_CONTACT', payload: id });
+    } catch (err) {
+      dispatch({ type: 'DELETE_CONTACT', payload: id });
+    }
+  };
+
+  render() {
+    const { id, name, email, phone } = this.props.contact;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className='card card-body mb-3'>
+              <h4>
+                {name}{' '}
+                <i
+                  onClick={() =>
+                    this.setState({
+                      showContactInfo: !this.state.showContactInfo
+                    })
+                  }
+                  className='fas fa-sort-down'
+                />
+                <i
+                  className='fas fa-times'
+                  style={{ color: 'red', float: 'right' }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+                <Link to={`contact/edit/${id}`}>
+                  <i
+                    className='fas fa-pencil-alt'
+                    style={{
+                      color: 'black',
+                      float: 'right',
+                      marginRight: '1rem'
+                    }}
+                  />
+                </Link>
+              </h4>
+              {this.state.showContactInfo ? (
+                <ul className='list-group'>
+                  <li className='list-group-item'>Email: {email}</li>
+                  <li className='list-group-item'>Phone: {phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+
+export default Contact;
